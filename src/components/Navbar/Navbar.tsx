11ea38a8650eadc7b1d607791c62ecdf0d67fe82
Juan Maldini <juan.maldini@outlook.com>
2026-02-01 23:20:09 -0300

    up

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import { useState } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
+import logo from "/public/logo.svg";
 import "./Navbar.css";
 
 const Navbar = () => {
@@ -8,6 +9,10 @@ const Navbar = () => {
 
   return (
     <header className="navbar-header">
+      <div className="nav-barnd">
+        <img src={logo} alt="Logo" className="logo" height={50}/>
+        Woloviz
+      </div>
       <nav className={`navbar${isMenuOpen ? " nav-open" : ""}`}>
         <Link to="/" className="nav-link" onClick={() => setIsMenuOpen(false)}>
           Home
