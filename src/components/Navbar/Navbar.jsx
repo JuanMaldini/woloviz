38ddda9f2166206705b0fe03e9557eecf7061524
Juan Maldini <juan.maldini@outlook.com>
2026-02-10 14:57:10 -0300

    up navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,36 +9,15 @@ const Navbar = () => {
 
   return (
     <header className="navbar-header">
-      <div className="nav-brand">
+      <Link to="/" className="nav-brand">
         {/* <img src={logo} alt="Logo" className="logo" height={50}/> */}
         Woloviz
-      </div>
+      </Link>
       <nav className={`navbar${isMenuOpen ? " nav-open" : ""}`}>
-        <Link to="/" className="nav-link" onClick={() => setIsMenuOpen(false)}>
-          Home
-        </Link>
-        <Link
-          to="/interactive-projects"
-          className="nav-link"
-          onClick={() => setIsMenuOpen(false)}
-        >
-          Projects
-        </Link>
-        <Link
-          to="/contact"
-          className="nav-link"
-          onClick={() => setIsMenuOpen(false)}
-        >
-          Contact
-        </Link>
+        <Link to="/interactive-projects" className="nav-link" onClick={() => setIsMenuOpen(false)}> Projects </Link>
+        <Link to="/contact" className="nav-link" onClick={() => setIsMenuOpen(false)}> Contact </Link>
       </nav>
-      <button
-        className="hamburger"
-        onClick={() => setIsMenuOpen(!isMenuOpen)}
-        aria-label="Toggle menu"
-      >
-        <GiHamburgerMenu />
-      </button>
+      <button className="hamburger" onClick={() => setIsMenuOpen(!isMenuOpen)} aria-label="Toggle menu"><GiHamburgerMenu /></button>
     </header>
   );
 };
