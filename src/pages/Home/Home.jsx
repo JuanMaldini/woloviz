62992d6e9710639f0ab4b063a93769f75e8fa8c9
Juan Maldini <juan.maldini@outlook.com>
2026-02-16 11:16:17 -0300

    home color

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,7 @@
 const Home = () => {
   return (
-    <div className="flex min-h-full w-full flex-1 flex-col items-center justify-center bg-gray-400 px-6 py-8">
-      <h1 className="mb-4 text-2xl font-semibold text-slate-200">Home</h1>
+    <div className="flex min-h-full w-full flex-1 flex-col items-center justify-center bg-gray-100">
+      <h1 className="mb-4 text-2xl font-semibold text-slate-800">Home</h1>
     </div>
   );
 };
