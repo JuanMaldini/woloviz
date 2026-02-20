d03deed0f81da14ac34cfbb06ca0b9580bc7124d
Juan Maldini <juan.maldini@outlook.com>
2026-02-19 22:15:02 -0300

    fragment

--- src/pages/Projects/ARSpace/ARSpace.jsx
+++ src/pages/Projects/ARSpace/ARSpace.jsx
@@ -3,14 +3,14 @@ import { Unity, useUnityContext } from "react-unity-webgl";
 
 const ARSpace = () => {
   const { unityProvider, loadingProgression, isLoaded } = useUnityContext({
-    loaderUrl: "unity/unity.loader.js",
-    dataUrl: "unity/unity.data.unityweb",
-    frameworkUrl: "unity/unity.framework.js.unityweb",
-    codeUrl: "unity/unity.wasm.unityweb",
+    loaderUrl: "/unity/Build/unity.loader.js",
+    dataUrl: "/unity/Build/unity.data.unityweb",
+    frameworkUrl: "/unity/Build/unity.framework.js.unityweb",
+    codeUrl: "/unity/Build/unity.wasm.unityweb",
   });
 
   return (
-    <Fragment>
+    <div className="flex min-h-full w-full flex-1 flex-col items-center justify-center bg-gray-100">
       {!isLoaded && (
         <p>Loading Application... {Math.round(loadingProgression * 100)}%</p>
       )}
@@ -18,8 +18,8 @@ const ARSpace = () => {
         unityProvider={unityProvider}
         style={{ visibility: isLoaded ? "visible" : "hidden" }}
       />
-    </Fragment>
+    </div>
   );
-}
+};
 
 export default ARSpace;
